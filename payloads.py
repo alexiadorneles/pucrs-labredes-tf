3d0a15609366e04cc914288c8c198002dbb1cb55
gustavo-lottermann <g.lottermann@hotmail.com>
2024-06-29 15:59:51 -0300

    codigo modificado

--- payloads.py
+++ payloads.py
@@ -2,132 +2,118 @@ import socket
 import binascii
 from enum import Enum
 
-class DHCPPayload:
+class DHCPFrame:
     def __init__(self, 
-                 opt, 
-                 htype,
-                 hlen,
-                 hops,
-                 trans_id,
-                 se,
+                 op_code, 
+                 hw_type,
+                 hw_len,
+                 hop_count,
+                 transaction_id,
+                 elapsed_seconds,
                  flags,
-                 ciaddr,
-                 yiaddr,
-                 siaddr,
-                 giaddr,
-                 chaddr,
-                 chaddr_padding,
-                 sname,
-                 bname,
-                 mcookie, 
-                 option):
-                #  option_53,
-                #  option_51,
-                #  option_1,
-                #  option_3,
-                #  option_6,
-                #  option_54,
-                #  option_125,
-                #  option_58,
-                #  option_59,
-                #  option_28,
-                #  option_255):
-        self._opt = opt
-        self._htype = htype
-        self._hlen = hlen
-        self._hops = hops
-        self._trans_id = trans_id
-        self._se = se
+                 client_ip,
+                 your_ip,
+                 server_ip,
+                 gateway_ip,
+                 client_hw_addr,
+                 hw_addr_padding,
+                 server_name,
+                 boot_file_name,
+                 magic_cookie, 
+                 option_type):
+        self._op_code = op_code
+        self._hw_type = hw_type
+        self._hw_len = hw_len
+        self._hop_count = hop_count
+        self._transaction_id = transaction_id
+        self._elapsed_seconds = elapsed_seconds
         self._flags = flags
-        self._ciaddr = ciaddr
-        self._yiaddr = yiaddr
-        self._siaddr = siaddr
-        self._giaddr = giaddr
-        self._chaddr = chaddr
-        self._chaddr_padding = chaddr_padding
-        self._sname = sname
-        self._bname = bname
-        self._mcookie = mcookie
-        self._option = option
-        # self._option_53 = option_53
-        # self._option_51 = option_51
-        # self._option_1 = option_1
-        # self._option_3 = option_3
-        # self._option_6 = option_6
-        # self._option_54 = option_54
-        # self._option_125 = option_125
-        # self._option_58 = option_58
-        # self._option_59 = option_59
-        # self._option_28 = option_28
-        # self._option_255 = option_255
+        self._client_ip = client_ip
+        self._your_ip = your_ip
+        self._server_ip = server_ip
+        self._gateway_ip = gateway_ip
+        self._client_hw_addr = client_hw_addr
+        self._hw_addr_padding = hw_addr_padding
+        self._server_name = server_name
+        self._boot_file_name = boot_file_name
+        self._magic_cookie = magic_cookie
+        self._option_type = option_type
 
-    def get_bytes(self):
-        return ( self._opt.to_bytes(1, 'little')
-            + self._htype.to_bytes(1, 'little')
-            + self._hlen.to_bytes(1, 'little')
-            + self._hops.to_bytes(1, 'little')
-            + self._trans_id + self._se.to_bytes(2, 'little')
-            + self._flags.to_bytes(2, 'little')
-            + socket.inet_pton(socket.AF_INET, self._ciaddr)
-            + socket.inet_pton(socket.AF_INET, self._yiaddr)
-            + socket.inet_pton(socket.AF_INET, self._siaddr)
-            + socket.inet_pton(socket.AF_INET, self._giaddr)
-            + binascii.unhexlify(self._chaddr)
-            + binascii.unhexlify(self._chaddr_padding)
-            + binascii.unhexlify(self._sname)
-            + binascii.unhexlify(self._bname) 
-            + binascii.unhexlify(self._mcookie)
-            + Options.get_options_by_package(self._option)
-            # + binascii.unhexlify(self._option_53) 
-            # + b'04' + binascii.unhexlify(self._option_51)
-            # + b'04' + binascii.unhexlify(self._option_1)
+    def to_bytes(self):
+        """
+        Converte a estrutura DHCPFrame em bytes.
+        """
+        return (self._op_code.to_bytes(1, 'little')
+                + self._hw_type.to_bytes(1, 'little')
+                + self._hw_len.to_bytes(1, 'little')
+                + self._hop_count.to_bytes(1, 'little')
+                + self._transaction_id 
+                + self._elapsed_seconds.to_bytes(2, 'little')
+                + self._flags.to_bytes(2, 'little')
+                + socket.inet_pton(socket.AF_INET, self._client_ip)
+                + socket.inet_pton(socket.AF_INET, self._your_ip)
+                + socket.inet_pton(socket.AF_INET, self._server_ip)
+                + socket.inet_pton(socket.AF_INET, self._gateway_ip)
+                + binascii.unhexlify(self._client_hw_addr)
+                + binascii.unhexlify(self._hw_addr_padding)
+                + binascii.unhexlify(self._server_name)
+                + binascii.unhexlify(self._boot_file_name)
+                + binascii.unhexlify(self._magic_cookie)
+                + DHCPOptions.get_options_by_type(self._option_type)
+                )
 
-            )
-            
-class EthernetPayload:
-	def __init__(self, destination, source, type):
-		self._destination = destination
-		self._source = source
-		self._type = type
+class EthernetFrame:
+    def __init__(self, dst, src, eth_type):
+        self._dst = dst
+        self._src = src
+        self._eth_type = eth_type
 
-class IPV4Payload:
-    def __init__(self, version, hl, tos, identification, flags, ttl, protocol, header_checksum, source, destination, ip_option):
-        self._version = version
-        self._hl = hl
+class IPv4Frame:
+    def __init__(self, ver, header_len, tos, ident, flags, ttl, proto, checksum, src, dst, options):
+        self._ver = ver
+        self._header_len = header_len
         self._tos = tos
-        self._identification = identification
+        self._ident = ident
         self._flags = flags
         self._ttl = ttl
-        self._protocol = protocol
-        self._header_checksum = header_checksum
-        self._source = source
-        self._destination = destination
-        self._ip_option = ip_option
+        self._proto = proto
+        self._checksum = checksum
+        self._src = src
+        self._dst = dst
+        self._options = options
 
-
-class Options(Enum):
+class DHCPOptions(Enum):
     OFFER = 'offer'
     ACK = 'ack'
 
     @staticmethod
-    def get_options_by_package(option):
-        if option == Options.OFFER:
-            return Options.get_offer_options()
-        elif option == Options.ACK:
-            return Options.get_ack_options()
+    def get_options_by_type(option_type):
+        """
+        Retorna as opções de DHCP de acordo com o tipo de pacote (OFFER ou ACK).
+        """
+        if option_type == DHCPOptions.OFFER:
+            return DHCPOptions.get_offer_options()
+        elif option_type == DHCPOptions.ACK:
+            return DHCPOptions.get_ack_options()
         return 
 
     @staticmethod
     def get_offer_options():
-        return (binascii.unhexlify(Options.option_53_offer() + Options.option_51() + Options.option_1() + Options.option_59())
-            + Options.option_3() + Options.option_6() + Options.option_54() + Options.option_28() + Options.option_255()
-        )
+        """
+        Retorna as opções para o pacote DHCP OFFER.
+        """
+        return (binascii.unhexlify(DHCPOptions.option_53_offer() + DHCPOptions.option_51() + DHCPOptions.option_1() + DHCPOptions.option_59())
+                + DHCPOptions.option_3() + DHCPOptions.option_6() + DHCPOptions.option_54() + DHCPOptions.option_28() + DHCPOptions.option_255()
+               )
     
     @staticmethod
     def get_ack_options():
-        return (binascii.unhexlify(Options.option_53_ack() + Options.option_51() + Options.option_1() + Options.option_59())
-            + Options.option_3() + Options.option_6() + Options.option_54() + Options.option_28() + Options.option_255()
-        )
+        """
+        Retorna as opções para o pacote DHCP ACK.
+        """
+        return (binascii.unhexlify(DHCPOptions.option_53_ack() + DHCPOptions.option_51() + DHCPOptions.option_1() + DHCPOptions.option_59())
+                + DHCPOptions.option_3() + DHCPOptions.option_6() + DHCPOptions.option_54() + DHCPOptions.option_28() + DHCPOptions.option_255()
+               )
     
     @staticmethod
     def option_53_offer():
@@ -143,19 +129,19 @@ class Options(Enum):
     
     @staticmethod
     def option_3():
-        return binascii.unhexlify('0304') + Options.get_ip()
+        return binascii.unhexlify('0304') + DHCPOptions.get_ip()
     
     @staticmethod
     def option_6():
-        return binascii.unhexlify('0604') + Options.get_ip()
+        return binascii.unhexlify('0604') + DHCPOptions.get_ip()
 
     @staticmethod
     def option_54():
-        return binascii.unhexlify('3604') + Options.get_ip()
+        return binascii.unhexlify('3604') + DHCPOptions.get_ip()
 
     @staticmethod
     def option_28():
-        return binascii.unhexlify('1c04') + Options.get_broadcast_ip()
+        return binascii.unhexlify('1c04') + DHCPOptions.get_broadcast_ip()
 
     @staticmethod
     def option_255():
@@ -175,4 +161,4 @@ class Options(Enum):
     
     @staticmethod
     def get_ip():
-        return socket.inet_pton(socket.AF_INET, '192.168.15.10')#socket.gethostbyname(socket.gethostname()))
+        return socket.inet_pton(socket.AF_INET, '192.168.15.10')
